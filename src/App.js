90c09e9688edd6608740851a128c38dcd23b0854
Jose Torres <vidojam@gmail.com>
2023-05-23 04:43:14 -0400

    Fixed borders

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ export default function App() {
         </header>
       </div>
     </BrowserRouter>
-    <Banner />
-    <Footer/>
+    {/* <Banner /> */}
+    {/* <Footer/> */}
     </>
   );
 }
--- src/scenes/Home.jsx
+++ src/scenes/Home.jsx
@@ -5,12 +5,12 @@ export default function Home () {
 
   return (
     <>
-    <box>
+    
       <h1 className="hometitle" >Toastmaster's Evaluator Assistant</h1>
       <img className = "homeimg" src="https://media.istockphoto.com/id/1441320232/photo/silhouette-of-man-hands-up-to-challenge-2022-on-mountain-hill-happy-new-year-2023-man.jpg?b=1&s=170667a&w=0&k=20&c=bKPkOBAs7bkdPNcyFw_gtOD4-etssQTRXtNxAzSujWg=" alt="" />
-      {/* <p className = "toastorg" >Toastmaster's International is a nonprofit educational organization that teaches public speaking and leadership skills through a worldwide network of clubs.</p> */}
+   
       <button className="homebtn" onClick={ () => navigate("/Eval-Entries") } >Go to Evaluation</button>
-    </box>
+    
     
     </>
   )

a299f56466fb91802a15a846585f55fe04ffcb96
Jose Torres <vidojam@gmail.com>
2023-05-28 10:21:31 -0400

    Removed TM on home page

--- src/scenes/Home.jsx
+++ src/scenes/Home.jsx
@@ -6,7 +6,7 @@ export default function Home () {
   return (
     <>
     
-      <h1 className="hometitle" >Toastmaster's Evaluator Assistant</h1>
+      <h1 className="hometitle" >Evaluator Assistant</h1>
       <img className = "homeimg" src="https://media.istockphoto.com/id/1441320232/photo/silhouette-of-man-hands-up-to-challenge-2022-on-mountain-hill-happy-new-year-2023-man.jpg?b=1&s=170667a&w=0&k=20&c=bKPkOBAs7bkdPNcyFw_gtOD4-etssQTRXtNxAzSujWg=" alt="" />
    
       <button className="homebtn" onClick={ () => navigate("/Eval-Entries") } >Go to Evaluation</button>
